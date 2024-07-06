36ca3b1cd6668a624323d1474b75ef1e1ac2eccb
kizataka <chongxiongmu@gmail.com>
2024-07-06 12:40:47 +0900

    ユーザー登録機能の実装

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,8 +1,40 @@
 import './styles.css';
+import { useState } from 'react';
+import axiosInstance from './axiosConfig';
 import { Header } from './components/Header';
 import { RegisterForm } from './components/RegisterForm';
+import { useNavigate } from 'react-router-dom';
 
 export const Register = () => {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirmation: ""
+  });
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axiosInstance.post("/api/account", formValues);
+      if (response.status === 201) {
+        console.log("ユーザー登録が完了しました");
+        navigate("/api/login");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("ユーザー登録に失敗しました");
+    }
+  }
+
   return (
     <>
       <Header />
@@ -10,7 +42,11 @@ export const Register = () => {
         <p>ユーザー登録</p>
       </div>
       <div className='input-area'>
-        <RegisterForm />
+        <RegisterForm
+          formValues={formValues}
+          onChange={onChange}
+          onSubmit={onSubmit}
+        />
       </div>
     </>
   );
