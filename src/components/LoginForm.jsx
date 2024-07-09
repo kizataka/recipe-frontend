e03f875fb074ff776864e06ca322a2925405dc0e
kizataka <chongxiongmu@gmail.com>
2024-07-09 11:49:44 +0900

    ログイン機能を実装

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,9 +1,10 @@
-export const LoginForm = () => {
-    return (
-      <form>
-        <input type="text" placeholder="メールアドレス" />
-        <input type="password" placeholder="パスワード" />
-        <button type="submit">ログイン</button>
-      </form>
-    );
-  }
+export const LoginForm = (props) => {
+  const { formValues, onChange, onSubmit } = props;
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" name="email" placeholder="メールアドレス" value={formValues.email} onChange={onChange} />
+      <input type="password" name="password" placeholder="パスワード" value={formValues.password} onChange={onChange}/>
+      <button type="submit">ログイン</button>
+    </form>
+  );
+};
