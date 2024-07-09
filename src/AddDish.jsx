3eee524efd1813fac2ceb0b6abbd1366368e749a
kizataka <chongxiongmu@gmail.com>
2024-07-09 11:49:14 +0900

    ログイン済ユーザーのみがレシピ投稿できる機能を実装

--- src/AddDish.jsx
+++ src/AddDish.jsx
@@ -1,17 +1,68 @@
 import './styles.css';
+import { useState, useEffect } from 'react';
+import { Header } from './components/Header';
+import axiosInstance from './axiosConfig';
 import { AddDishForm } from './components/AddDishForm';
+import { useNavigate } from 'react-router-dom';
 
 export const AddDish = () => {
+  const [formValues, setFormValues] = useState({
+    title: "",
+    description: "",
+    // image: ""
+  });
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      alert("ログインが必要です");
+      navigate("/api/login");
+    }
+  }, [navigate]);
+
+  const onChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (formValues.title === "" || formValues.description === "") {
+      alert("全ての項目を入力してください");
+      return;
+    }
+    try {
+      const token = localStorage.getItem("token")
+      const response = await axiosInstance.post("/api/dishes", formValues, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      if (response.status === 200) {
+        console.log("レシピの登録が完了しました");
+        alert("レシピの登録が完了しました");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("レシピの登録に失敗しました");
+    }
+  };
+
   return (
     <>
+      <Header />
       <div>
-        <h1>レシピ共有アプリ</h1>
-      </div>
-      <div>
-        <p>レシピの投稿・編集・削除</p>
+        <h1>レシピの投稿</h1>
       </div>
       <div className='input-area'>
-        <AddDishForm />
+        <AddDishForm
+          formValues={formValues}
+          onChange={onChange}
+          onSubmit={onSubmit}
+        />
       </div>
     </>
   );
