3d77bb51523ef19f146ec31137d8938af1f5d550
kizataka <chongxiongmu@gmail.com>
2024-07-11 16:11:16 +0900

    ユーザー詳細ページへの遷移機能を追加

--- src/components/DishDetailView.jsx
+++ src/components/DishDetailView.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 export const DishDetailView = ({ dish }) => {
     return (
         <div className="dish-detail-container">
             <div className="left-side">
                 <p>レシピの名前：{dish.title}</p>
-                <p>作成者：{dish.user.name}</p>
+                <p>作成者：<Link to={`/api/userDetail/${dish.user.id}`}>{dish.user.name}</Link></p>
                 <p>レシピの画像：</p>
                 <img src="https://img.icons8.com/ios/50/000000/image.png" alt="レシピの画像" />
             </div>
