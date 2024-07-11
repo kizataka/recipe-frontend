70561ccb2d4752a5f1a17a451f39bed49a5d7573
kizataka <chongxiongmu@gmail.com>
2024-07-11 15:50:46 +0900

    ユーザーデータを取得

--- src/UserDetail.jsx
+++ src/UserDetail.jsx
@@ -2,15 +2,37 @@ import './styles.css';
 import { Header } from './components/Header';
 import { UserDetailView } from './components/UserDetailView';
 import { UserDishList } from './components/UserDishList';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axiosInstance from './axiosConfig';
 
 export const UserDetail = () => {
+    const { id } = useParams();
+    const [user, setUser] = useState();
+
+    useEffect (() => {
+        const fetchUserDetail = async () => {
+            try {
+                const response = await axiosInstance.get(`/api/users/${id}`);
+                if (response.status === 200) {
+                    console.log("ユーザー詳細の取得に成功しました");
+                    console.log(response.data);
+                    setUser(response.data);
+                }
+            } catch (error) {
+                console.error("ユーザー詳細の取得に失敗しました", error);
+            }
+        };
+        fetchUserDetail();
+    }, [id]);
+
     return (
         <>
             <Header />
             <div>
                 <h2>ユーザー詳細</h2>
             </div>
-            <UserDetailView />
+            {user && <UserDetailView user={user} />}
             <UserDishList />
         </>
     );
--- src/components/UserDetailView.jsx
+++ src/components/UserDetailView.jsx
@@ -1,8 +1,8 @@
-export const UserDetailView = () => {
+export const UserDetailView = ({ user }) => {
     return (
         <div>
-            <p>ユーザー名</p>
-            <p>メールアドレス</p>
+            <p>ユーザー名：{user.name}</p>
+            <p>メールアドレス：{user.email}</p>
         </div>
     );
 }
