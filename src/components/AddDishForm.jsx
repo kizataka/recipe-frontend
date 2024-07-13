ccd1003fd4dcccd80ac54f5ada1ae5f1a219ed9a
kizataka <chongxiongmu@gmail.com>
2024-07-13 15:27:58 +0900

    編集と再投稿の機能を追加

--- src/components/AddDishForm.jsx
+++ src/components/AddDishForm.jsx
@@ -2,10 +2,9 @@ export const AddDishForm = (props) => {
   const { formValues, onChange, onSubmit } = props;
     return (
       <form onSubmit={onSubmit}>
-        {/* <input type="image" name="image" src="https://img.icons8.com/ios/50/000000/image.png" alt="アップロード" value={formValues.image} onChange={onChange} /> */}
         <input type="text" name="title" placeholder="レシピ名" value={formValues.title} onChange={onChange} />
         <textarea type="text" name="description" placeholder="レシピの説明" value={formValues.description} onChange={onChange} rows="20"/>
-        <button type="submit">投稿</button>
+        <button type="submit">{formValues.title ? "再投稿" : "投稿"}</button>
       </form>
     );
   }
