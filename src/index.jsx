198ab1c3ad87c44352c6c3db9ca3b257beb4ba1b
kizataka <chongxiongmu@gmail.com>
2024-07-18 06:06:50 +0900

    reportwebvitalsの削除

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,6 @@ import { AddDish } from './AddDish';
 import { DishDetail } from './DishDetail';
 import { UserDetail } from './UserDetail';
 import { DishList } from './DishList';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -25,8 +24,3 @@ root.render(
     </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
